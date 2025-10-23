e4602cf185f6009de2581f6a35872307af3c84c2
applegod75 <chrsbuitendijk@gmail.com>
2025-10-23 10:48:10 +0200

    added targets
    It's not very elegant yet, so I'll have to tune it up a bit later.  This was mostly just meant as a quick QOL update

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@
 std::ifstream reader;
 std::ofstream writer;
 std::string filename;
+std::string target;
 using json = nlohmann::json;
 json j;
 
 int main(int argc, char* argv[]) {
-	if (argc == 2) {
+	if (argc >= 2) {
 		reader.open(std::string(argv[1]));
 		if (reader.fail()) {
 			std::cout << "reader failed";
@@ -22,6 +23,8 @@ int main(int argc, char* argv[]) {
 	} else {
 		std::cout << "filename >> ";
 		std::cin >> filename;
+		std::cout << "target filename >> ";
+		std::cin >> target;
 		reader.open(filename);
 		if (reader.fail()) {
 			std::cout << "reader failed" << '\n';
@@ -35,7 +38,19 @@ int main(int argc, char* argv[]) {
 		throw(e);
 		return EXIT_FAILURE;
 	}
-	writer.open("extract" + filename);
+	if (argc > 2 and std::string(argv[2]) == "-t") {
+		writer.open(std::string(argv[3]));
+		if (writer.fail()) {
+			std::cerr << "writer failed, writing to 'extract" << filename << "'\n";
+			writer.open("extract" + filename);
+		}
+	} else {
+		if (argc < 3 and argc != 1)
+			writer.open("extract" + filename);
+		if (argc == 1) {
+			writer.open(target);
+		}
+	}
 	writer << std::setw(4) << j;
 	return 0;
 }
